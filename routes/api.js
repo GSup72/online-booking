1d04273d48d42515545280aa59fa783168c50a93
Denis Blaga <blaha.denys@student.uzhnu.edu.ua>
2023-10-13 15:04:00 +0300

    Edit api

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+router.get('/', (req, res) => {
+	res.status(404);
+});
 router.get('/test', (req, res) => {
 	res.json({ message: '/api/test' });
 });
